701ffacc8ecac068057150ec2b62cf104d7c8ce6
Renée Kooi <renee@kooi.me>
2018-06-30 17:12:56 +0200

    Export commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import pkg from './package.json';
 export default {
 	input: 'src/index.ts',
 	output: [
-		{ file: pkg.main, format: 'umd' },
+		{ file: pkg.main, format: 'cjs' },
 		{ file: pkg.module, format: 'es' }
 	],
 	name: 'tape',
